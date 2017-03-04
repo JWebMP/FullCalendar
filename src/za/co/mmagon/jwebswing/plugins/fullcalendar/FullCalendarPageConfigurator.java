9e70a62a1f4c7baf33afae5dea4eaa32fc926807
Marc Magon <ged_marc@hotmail.com>
2017-03-04 09:24:24 +0200

    v0.9

--- src/za/co/mmagon/jwebswing/plugins/fullcalendar/FullCalendarPageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/fullcalendar/FullCalendarPageConfigurator.java
@@ -1,10 +1,10 @@
-/*
- * Copyright (C) 2017 GedMarc
+/* 
+ * Copyright (C) 2017 Marc Magon
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.mmagon.jwebswing.plugins.fullcalendar;
 
@@ -28,11 +27,23 @@ import za.co.mmagon.jwebswing.plugins.PluginInformation;
  * @since 23 Feb 2017
  *
  */
-@PluginInformation(pluginName = "Full Calendar", pluginUniqueName = "full-calendar", pluginDescription = "", pluginVersion = "3.2.0",
-        pluginDependancyUniqueIDs = "moment", pluginCategories = "jquery, calendar, schedular, event planner, events, ui, web", pluginSubtitle = "Display a full-size drag-n-drop event calendar, leveraging jQuery. ",
-        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-FullCalendarPlugin", pluginSourceUrl = "",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-FullCalendarPlugin/wiki",
-        pluginOriginalHomepage = "https://fullcalendar.io/")
+@PluginInformation(pluginName = "Full Calendar",
+                   pluginUniqueName = "full-calendar",
+                   pluginDescription = "Full Calendar is a free, open-source project that enables you to design full calendar-like features and functions that is fully interactive, ajax controlled and mobile friendly. ",
+                   pluginVersion = "3.2.0",
+                   pluginDependancyUniqueIDs = "jquery,moment",
+                   pluginCategories = "jquery, calendar, schedular, event planner, events, ui, web",
+                   pluginSubtitle = "Display a full-size drag-n-drop event calendar, leveraging jQuery. ",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebSwing-FullCalendarPlugin",
+                   pluginSourceUrl = "https://github.com/fullcalendar",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-FullCalendarPlugin/wiki",
+                   pluginOriginalHomepage = "https://fullcalendar.io/",
+                   pluginSourceDonateUrl = "https://fullcalendar.io/donate/",
+                   pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/FullCalendarPlugin.jar/download",
+                   pluginIconUrl = "bower_components/fullcalendar/fullcalendar_icon.png",
+                   pluginIconImageUrl = "bower_components/fullcalendar/fullcalendar_example.png",
+                   pluginLastUpdatedDate = "2017/03/04"
+)
 public class FullCalendarPageConfigurator extends PageConfigurator
 {
 
