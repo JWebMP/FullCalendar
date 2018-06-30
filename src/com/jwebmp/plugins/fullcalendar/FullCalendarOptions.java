9e298ddbe06dda3b198063ac957e4b8b6b76dc66
GedMarc <ged_marc@hotmail.com>
2018-06-30 18:42:48 +0200

    Package Update to not confuse with google guice, module info updates

--- src/com/jwebmp/plugins/fullcalendar/FullCalendarOptions.java
+++ src/com/jwebmp/plugins/fullcalendar/FullCalendarOptions.java
@@ -19,7 +19,7 @@ package com.jwebmp.plugins.fullcalendar;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jwebmp.base.servlets.interfaces.IDataComponent;
-import com.jwebmp.guiceinjection.GuiceContext;
+import com.jwebmp.guicedinjection.GuiceContext;
 import com.jwebmp.htmlbuilder.javascript.JavaScriptPart;
 import com.jwebmp.plugins.fullcalendar.options.FullCalendarEventsList;
 import com.jwebmp.plugins.fullcalendar.options.FullCalendarHeaderOptions;
