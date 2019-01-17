489cffc818866c32e3634b88ecf15c1bc9bc0df5
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:00:46 +0200

    GPG Update

--- src/main/java/com/jwebmp/plugins/fullcalendar/options/FullCalendarGoogleCalendarEventSource.java
+++ src/main/java/com/jwebmp/plugins/fullcalendar/options/FullCalendarGoogleCalendarEventSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ package com.jwebmp.plugins.fullcalendar.options;
 import com.jwebmp.plugins.fullcalendar.FullCalendar;
 
 /**
- * @author Marc Magon
+ * @author GedMarc
  * @since 04 Apr 2017
  */
 public class FullCalendarGoogleCalendarEventSource
--- src/main/java/com/jwebmp/plugins/fullcalendar/options/IFullCalendarEvent.java
+++ src/main/java/com/jwebmp/plugins/fullcalendar/options/IFullCalendarEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ package com.jwebmp.plugins.fullcalendar.options;
 import java.io.Serializable;
 
 /**
- * @author Marc Magon
+ * @author GedMarc
  * @since 04 Apr 2017
  */
 public interface IFullCalendarEvent
